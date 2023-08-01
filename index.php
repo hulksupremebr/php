4734bc1a21283e7748bf230328427ecf90e8c313
vitoriatakeshita <115591777+hulksupremebr@users.noreply.github.com>
2023-08-01 14:38:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!"; //echo serve pra mostrar algo na tela
+$turma2 = "Vamos desenvolver website em PHP";
+echo "<br>" . $turma2;
+$x = 5 /* + 15 */ + 5;
+echo "<br>" . $x;
+
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+echo "<br>". $txt."<br>"."x = ".$x. " y = ".$y
+
+?>
+
+</body>
+</html> 
